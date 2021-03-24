93c55a343b037f816582a65f7c65be603ccfb25b
StevenAdema <stevenadema@gmail.com>
2021-03-23 22:41:48 -0400

    preferred time slot handling

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# ymca-auto-register-bot
-A bot to automate the login and booking of workouts sessions or classes at the YMCA
+
+# 🤖 ymca-auto-booker
+
+A selenium-python app that automatically enrolls you in your preferred workout session (and soon group classes) at 
+YMCA-YWCA National Capital Region locations.
+
+## Background
+
+As of March 19, 2021, Ottawa is in lockdown level red limiting gym capacity to just 10 persons per a session.  The YMCA 
+allows limited booking of their classes daily at 9 AM for up to 7 days in advance. The most desirable slots are often 
+booked within minutes of becoming available. This bot automatically logs in navigates through the web forms to create a 
+booking in less than 30 seconds.
+
+## Demo
+
+![YMCA auto-booking selenium bot](/config/demo.gif?raw=true)
+
+## Installation
+1. Clone the repo.
+2. ``` pip install -r requirements.txt ```
+3. Deploy via Heroku
+4. Use Heroku Scheduler to run app.py daily at 9:00 AM EST
+
+
--- app.py
+++ app.py
@@ -8,51 +8,50 @@ import datetime as dt
 
 def main():
     # get credentials
-    with open(r'config\account-credentials.yaml') as f:
+    with open(r'config\config.yaml') as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
 
     # variables
     email = cfg['email']
     password = cfg['password']
-    time_slot = cfg['preferred_time_slot']
     location = cfg['location']
-    booking_date = get_booking_date(9)
-    path = r'C:\Program Files\Google\chromedriver.exe'
+    time_slot = get_time_slot(cfg)
+    # booking_date = get_booking_date(9)
+    booking_date = 'March 25, 2021'
+    path = cfg['chrome_driver_path']
     driver = webdriver.Chrome(path)
 
     driver.get('https://ymcaywca.legendonlineservices.ca/enterprise/account/login')
     time.sleep(3)
 
+    # login to YMCA
     search = driver.find_element_by_id('account-login-email')
     search.send_keys(email)
-
     search = driver.find_element_by_id('account-login-password')
     search.send_keys(password)
     time.sleep(0.5)
-
     search.send_keys(Keys.RETURN)
     time.sleep(1)
 
+    # open booking page and select location, workout
     driver.get('https://ymcaywca.legendonlineservices.ca/enterprise/bookingscentre/index')
-    time.sleep(3)
-
+    time.sleep(3.5)
     select = driver.find_element_by_class_name('select2-search__field')
     select.send_keys(location)
     time.sleep(0.5)
     select.send_keys(Keys.RETURN)
     time.sleep(1)
-
     radio = driver.find_element_by_id('booking-behaviour-option373')
     radio.click()
     time.sleep(1)
-
     checkbox = driver.find_element_by_id('booking-activity-option0372')
     checkbox.click()
     time.sleep(1)
-
-    btn = driver.find_element_by_class_name('btn-success').click()
+    btn = driver.find_element_by_class_name('btn-success')
+    btn.click()
     time.sleep(1)
 
+    # select workout date and slot
     booking = driver.find_element_by_id('unique-identifier-2')
     time.sleep(0.5)
     booking.send_keys(Keys.CONTROL + 'a')
@@ -63,23 +62,41 @@ def main():
     time.sleep(0.5)
     booking.send_keys(Keys.RETURN)
     time.sleep(1)
-
-    select_workout = driver.find_elements_by_xpath('//*[@id="bookingPage"]/bookings-timetable/div/div[10]/a/div/div/div[1]/div[1]')[0]
+    select_workout = driver.find_elements_by_xpath(time_slot)[0]
     select_workout.click()
     time.sleep(1)
 
+    # add to cart
     add_to_basket = driver.find_elements_by_xpath('//*[@id="timeTableDetails"]/div/div/div[4]/button[3]')[0]
     add_to_basket.click()
-    time.sleep(30)
+    time.sleep(5)
 
+    basket = driver.find_elements_by_xpath('/html/body/div/div[2]/div/universal-basket-summary/div[2]/form/div[2]/div[2]/universal-basket-options/universal-basket-continue-options/button[1]')[0]
+    basket.click()
+    time.sleep(2)
+
+    terms_checkbox = driver.find_element_by_xpath('/html/body/div/div[2]/div/universal-basket-summary/div[2]/form/div[2]/div[1]/div[4]/universal-basket-payment-details/div/div/div[4]/universal-basket-terms-conditions/ul/li/div[1]/label/input')
+    terms_checkbox.click()
+    time.sleep(10)
+
+    # close driver
     driver.quit()
 
 
+def get_time_slot(cfg):
+    ''' Convert the preferred time slot to the appropriate div from the web form '''
+    preferred_time_slot = cfg['preferred_time_slot']
+    time_slot_div = str(cfg['timeslots_dict'][preferred_time_slot])
+    time_slot_xpath = '/html/body/div[1]/div[2]/div/div/bookings-timetable/div/div[' + time_slot_div + ']'
+    return time_slot_xpath
+
+
 def get_booking_date(delta):
+    ''' Add 7 days to today's date in the appropriate format '''
     booking_date = dt.date.today() + dt.timedelta(days=delta)
-    print(booking_date)
     booking_date = booking_date.strftime('%B %d, %Y')
     return booking_date
+
 
 if __name__ == '__main__':
     main()
