c631077505d9155d65e99ccfea8312ccb957c06a
StevenAdema <stevenadema@gmail.com>
2021-03-25 21:34:06 -0400

    config update

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def run_locally(cfg, local):
     else:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
