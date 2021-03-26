fd429ab4d7ce55ad0e96f9b6c654011fa8378696
StevenAdema <stevenadema@gmail.com>
2021-03-25 20:46:37 -0400

    config update

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def main():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver =  webdriver.Chrome(executable_path=os.environ.get("CHROME DRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     # variables
     email = cfg['email']
